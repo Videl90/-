114c90831be75e30d801e4e551e50958d4c8e078
Videl90 <lorena.sa.0117@gmail.com>
2020-08-25 22:07:05 -0500

    Updates

---
+++ precache-manifest.973bd025f1e8c0d0a4e2115923bf8884.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d54e85885af0bd182fd00a993b16cb",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "e116d7b8dc2b319d65b6",
+    "url": "/react_portfolio/static/css/main.7cb6c89e.chunk.css"
+  },
+  {
+    "revision": "a35ec78364829bec4e90",
+    "url": "/react_portfolio/static/js/2.c4e0aec7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_portfolio/static/js/2.c4e0aec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e116d7b8dc2b319d65b6",
+    "url": "/react_portfolio/static/js/main.2536d1d6.chunk.js"
+  },
+  {
+    "revision": "73d380a9d5f5561f4586",
+    "url": "/react_portfolio/static/js/runtime-main.44796f72.js"
+  },
+  {
+    "revision": "4266e382f9b3f26f04bc5505e5815940",
+    "url": "/react_portfolio/static/media/concerts-1.4266e382.png"
+  },
+  {
+    "revision": "e472af83131c24a0255a8930d54533c0",
+    "url": "/react_portfolio/static/media/dibujo-1.e472af83.png"
+  },
+  {
+    "revision": "56f839f7ba17b29e0ddbb389c38045c8",
+    "url": "/react_portfolio/static/media/digital-1.56f839f7.png"
+  },
+  {
+    "revision": "1a4ae3d0bc7d0207e93377af41b83c60",
+    "url": "/react_portfolio/static/media/hover-concerts-1.1a4ae3d0.png"
+  },
+  {
+    "revision": "176d340551ba3823829cdff4f6fcd591",
+    "url": "/react_portfolio/static/media/hover-dibujo-1.176d3405.png"
+  },
+  {
+    "revision": "eaab2258626d8693c023577c30d0ce35",
+    "url": "/react_portfolio/static/media/hover-digital-1.eaab2258.png"
+  },
+  {
+    "revision": "d901675ef137d71d36e94b407b0c2839",
+    "url": "/react_portfolio/static/media/hover-nature-1.d901675e.png"
+  },
+  {
+    "revision": "d6e9a0495e6ca31b2daf43688e63fe46",
+    "url": "/react_portfolio/static/media/hover-painting-1.d6e9a049.png"
+  },
+  {
+    "revision": "c08a192dee04c565a955d694387c536d",
+    "url": "/react_portfolio/static/media/hover-theatre-1.c08a192d.png"
+  },
+  {
+    "revision": "deb59de7a14b612b6a99904455a4808a",
+    "url": "/react_portfolio/static/media/hover-web-1.deb59de7.png"
+  },
+  {
+    "revision": "3fd733061010e9dd2fa941fcbc67c92a",
+    "url": "/react_portfolio/static/media/nature-1.3fd73306.png"
+  },
+  {
+    "revision": "783f0d21441e0d68db90ee5e0f5f034a",
+    "url": "/react_portfolio/static/media/painting-1.783f0d21.png"
+  },
+  {
+    "revision": "e8cd90fa5590105ce4e24fdc7f218ef1",
+    "url": "/react_portfolio/static/media/theatre-1.e8cd90fa.png"
+  },
+  {
+    "revision": "40c9af1247dc7bc9912f4dde4b3647a2",
+    "url": "/react_portfolio/static/media/w1.40c9af12.png"
+  },
+  {
+    "revision": "27e844e60a53571c57987b5a472ffbb6",
+    "url": "/react_portfolio/static/media/w10.27e844e6.png"
+  },
+  {
+    "revision": "455486982dbbec771d77471388e8c80a",
+    "url": "/react_portfolio/static/media/w2.45548698.png"
+  },
+  {
+    "revision": "e9ca1e70ae2a9c74de0422bd39963650",
+    "url": "/react_portfolio/static/media/w3.e9ca1e70.png"
+  },
+  {
+    "revision": "a642a7e3b2525db4f359d2bdb0b29a14",
+    "url": "/react_portfolio/static/media/w4.a642a7e3.png"
+  },
+  {
+    "revision": "f579f12f4647233a02449454625e9f33",
+    "url": "/react_portfolio/static/media/w5.f579f12f.png"
+  },
+  {
+    "revision": "1a4adc5f4dbf48bcf1a9ee36c816189e",
+    "url": "/react_portfolio/static/media/w6.1a4adc5f.png"
+  },
+  {
+    "revision": "f1cdf4a2ac086344559d52139634e994",
+    "url": "/react_portfolio/static/media/w7.f1cdf4a2.png"
+  },
+  {
+    "revision": "91ea4e73fa8c2fa9d9f17b25563e65c2",
+    "url": "/react_portfolio/static/media/w8.91ea4e73.png"
+  },
+  {
+    "revision": "f20a18ad48286726c72ffca58812c270",
+    "url": "/react_portfolio/static/media/w9.f20a18ad.png"
+  },
+  {
+    "revision": "d024391bfa5b6cafd4d2ca888c98ecc8",
+    "url": "/react_portfolio/static/media/web-1.d024391b.png"
+  },
+  {
+    "revision": "e4a751461b75052d22de53e9b8eddfb4",
+    "url": "/react_portfolio/static/media/yo.e4a75146.png"
+  }
+]);
